d8c15c8c4b2fdf0e0e6fd48cb7506aee2be1a5f7
Francisca <franciscaljubetic@gmail.com>
2021-11-24 20:56:07 -0300

    nueva apariencia no terminada

---
+++ myscript.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+    
+    $('#gifi').hide();
+    $('#textloading').html('click to start');
+    
+
+    $( "#titulo_neon" ).on('click', 
+        function() {
+            $( "#primera_pantalla" ).hide();
+        
+        }
+    );
+
+    
+
+    
+    
+    $( "#contacto" ).on('click', 
+        function() {
+        $( "#cajaformulario").fadeIn();
+        }
+    );
+    
+    $( "#nosotros" ).hover( 
+        function() {
+        $( "#nosotrostexto" ).toggle();
+        }
+    );
+
+    $( "#vision" ).hover( 
+        function() {
+        $( "#visiontexto" ).toggle();
+        }
+    );
+
+    $( "#mision" ).hover( 
+        function() {
+        $( "#misiontexto" ).toggle();
+        }
+    );
+
+
+    $( "#valores" ).hover( 
+        function() {
+        $( "#valorestexto" ).toggle();
+        }
+    );
+
+    $( "#objestrategicos" ).hover( 
+        function() {
+        $( "#objestrategicostexto" ).toggle();
+        }
+    );
+
+    $( "#servicios" ).on('click',
+        function() {
+        $( "#serviciostexto" ).toggle();
+        }
+    );
+
+    
+
+    $('#botonenviar').on('click',function(evento){
+        evento.preventDefault();
+
+        $(".toast").toast("show");
+    });
+
+            
+})
+
+/*jQuery(document).ready(function( $ ) {
+$('.fondo-efecto-ondas').ripples({
+    resolution: 512,
+    dropRadius: 20,
+    perturbance: 0.01,
+});
+});*/
+
